44940597c129a5afd6be345b33e708e4cab5b7ca
kylebunting <kyle.bunting@gmail.com>
2020-02-23 19:02:42 -0500

    Config script updates for issue #28

--- Hands-on lab/lab-files/LabVM/labvmconfig.sh
+++ Hands-on lab/lab-files/LabVM/labvmconfig.sh
@@ -30,10 +30,12 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 apt-get install -y nodejs
 
 #install MongoDB Community Edition
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
-apt-get update && sudo apt-get install -y mongodb-org
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update && sudo apt-get install -y mongodb-org
 sudo service mongod start
+sudo systemctl enable mongod.service
 
 #install docker
 #to run the az cli container open terminal and use 'sudo docker run -it azuresdk/azure-cli-python:latest'
